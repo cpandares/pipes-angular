aa8c9a6143582195f67ea052c1f35ea3e6736c4d
cpandares <pandaresocesar@gmail.com>
2024-07-17 09:39:29 -0400

    rutas para pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { PrimeIcons, MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipe-app';
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+      this.primengConfig.ripple = true;
+  }
+
+
 }
